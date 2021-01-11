fabb753482eb407b95c78509718acbec946e1bdd
Alex Zaytsev <pogruzchik88@gmail.com>
2021-01-11 22:19:59 +0300

    added code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12600;
+        int onemile = 20;
+        int freemiles = ticketPrice / onemile;
+        System.out.println(freemiles);
+    }
+}
